7e22af4d6f44c15c40db56f4cd1b3df55a737450
xfazze <blixtvinge@gmail.com>
2023-04-16 22:02:28 +0200

    WOrking version of league customt

--- src/cogs/leagueCustoms.py
+++ src/cogs/leagueCustoms.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 import sqlite3
 from numpy import log
+import datetime
 
 
 class leagueCustoms(commands.Cog):
@@ -25,24 +26,38 @@ class leagueCustoms(commands.Cog):
 
         dbcon = sqlite3.connect("data.db")
         dbcur = dbcon.cursor()
-        res = dbcur.execute("CREATE TABLE IF NOT EXISTS players(discord_id UNIQUE, rating, timestamp DEFAULT CURRENT_TIMESTAMP)")
+        res = dbcur.execute("CREATE TABLE IF NOT EXISTS players(discord_id, rating, timestamp)")
         # print("res of creatint", res.fetchone())
         # tables = dbcur.execute("SELECT name FROM sqlite_master")
         # print("after crea",tables.fetchone())
         
         # Create players if not existsing
-        formatted_players = [(str(player.id), str(1000)) for player in players]
-        dbcur.executemany('INSERT OR IGNORE INTO players VALUES(?, ?)',formatted_players)
+        formatted_players = []
+        new_players = []
+        for player in players:
+            res = dbcur.execute(f"SELECT discord_id, rating FROM players WHERE discord_id='{player.id}' ORDER BY timestamp DESC LIMIT 1")
+            pla = res.fetchone()
+            print("pla  res:",pla)
+            if not pla:
+                pla = (str(player.id), str(1000),datetime.datetime.now() )
+                new_players.append(pla)
+            formatted_players.append(pla)
+        print("new pla", new_players)
+        dbcur.executemany('INSERT  INTO players VALUES(?, ?, ?) ',new_players)
         dbcon.commit()
         dbcon.close()
+        print("updateed")
         
-        formatted_players.sort(key=lambda a:int(a[1]))
+        formatted_players.sort(key=lambda a:float(a[1]))
+        print("formatted.players", formatted_players)
         team_left = []
         team_right = []
         while len(formatted_players) != 0:
-            if sum([int(p[1]) for p in team_left]) > sum([int(p[1]) for p in team_right]):
+            if sum([float(p[1]) for p in team_left]) > sum([float(p[1]) for p in team_right]):
+                print("add team righ", formatted_players[-1])
                 team_right.append(formatted_players.pop())
             else:
+                print("add team left", formatted_players[-1])
                 team_left.append(formatted_players.pop())
         
         embed = discord.Embed(
@@ -66,38 +81,59 @@ class leagueCustoms(commands.Cog):
                     await interaction.response.edit_message(embed=embed, view=None)
                     return
                 try:
-                    embed.title = f"{interaction.data['custom_id']} Team Winner"
-                    await interaction.response.edit_message(embed=embed, view=None)
-                    mmr_diff=abs([int(p[1]) for p in team_left] -[int(p[1]) for p in team_right])
-                    mmr_diff_maxed = max(abs(mmr_diff), 100)/100  # 0 to 1
-                    mmr_diff_powed = mmr_diff_maxed**2       
-                    mmr_diff_scaled = 1+ mmr_diff_powed * mmr_diff/abs(mmr_diff) # 0 to 2. over 1 when left is higher mmr
+                    #print("p", team_right)
+                    mmr_diff=abs(sum([int(p[1]) for p in team_left]) -sum([int(p[1]) for p in team_right]))
+                    mmr_diff_maxed = max(min(abs(mmr_diff), 100)/100,10)  # 0 to 1
+                    mmr_diff_powed = mmr_diff_maxed**2     
+                    mmr_diff_scaled = 1+ mmr_diff_powed * 1 if mmr_diff == 0 else mmr_diff/abs(mmr_diff) # 0 to 2. over 1 when left is higher mmr
+                    print("mmr diff scaled")
+                except Exception as e:
+                    print("error", e)
+                print("PHASE 1 DONE")
                     
                     
+                try:
                     dbcon = sqlite3.connect("data.db")
                     dbcur = dbcon.cursor()
                     
                     updated_players = []
                     for left_player in team_left:
                         res = dbcur.execute(f"SELECT discord_id, rating FROM players WHERE discord_id='{left_player[0]}' ORDER BY timestamp DESC LIMIT 1")
-                        player = res.fetchone()[0]
-                        
+                        player =list( res.fetchone())
+                        player[1] = float(player[1])
                         player[1] +=  10*(2-mmr_diff_scaled) if interaction.data['custom_id']== "Left" else -10*mmr_diff_scaled
+                        player[1] = str(player[1])
                         
                         updated_players.append(player)
                     
                     for right_player in team_right:
                         res = dbcur.execute(f"SELECT discord_id, rating FROM players WHERE discord_id='{right_player[0]}' ORDER BY timestamp DESC LIMIT 1")
-                        player = res.fetchone()[0]
+                        player =list( res.fetchone())
+
                         
+                        player[1] = float(player[1])
                         player[1] +=  -10 * (2-mmr_diff_scaled) if interaction.data['custom_id']== "Left" else 10*mmr_diff_scaled
+                        player[1] = str(player[1])
                         updated_players.append(player)
                         
-                    dbcur.executemany('INSERT OR IGNORE INTO players VALUES(?, ?)',updated_players)
-                    dbcur.commit()            
-                    dbcon.close()
                 except Exception as e:
                     print("error", e)
+                print("PHASE 2 DONE")
+                
+                try:
+                    for player in updated_players:
+                        print("new palyer mmr", player)
+                        player.append( datetime.datetime.now())
+                        
+                    dbcur.executemany('INSERT INTO players VALUES(?, ?, ?)',updated_players)
+                    dbcon.commit()            
+                    dbcon.close()
+                    embed.title = f"{interaction.data['custom_id']} Team Winner & mmr updtaed"
+                    await interaction.response.edit_message(embed=embed, view=None)
+                    
+                except Exception as e:
+                    print("error", e)
+                print("PHASE 3 DONE")
                     
 
         
--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ import asyncio
 import discord
 from discord.ext import commands
 from cogwatch import Watcher
+import logging
+
+discord.utils.setup_logging(level=logging.INFO, root=False)
 
 load_dotenv()
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="¤")
@@ -13,10 +16,10 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix="¤")
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
-    if os.getenv("DEV") == "TRUE":
-        print("DEV MODE ACTIVE")
-        watcher = Watcher(bot, path="cogs", preload=True, debug=True)
-        await watcher.start()
+    # if os.getenv("DEV") == "TRUE":
+    #     print("DEV MODE ACTIVE")
+    #     watcher = Watcher(bot, path="cogs", preload=True, debug=True)
+    #     await watcher.start()
 
 
 @bot.command()
@@ -25,16 +28,19 @@ async def alive(ctx):
 
 
 async def main():
+    if os.getenv("DEV") != "TRUE":
+        await bot.load_extension("cogs.info")
+        await bot.load_extension("cogs.betterVC")
+        await bot.load_extension("cogs.autoPublic")
+        await bot.load_extension("cogs.leagueCustoms")  
+    else:
+
     # await bot.load_extension("cogs.dev")
-    await bot.load_extension("cogs.info")
-    await bot.load_extension("cogs.betterVC")
-    await bot.load_extension("cogs.autoPublic")
-    # await bot.load_extension("cogs.leagueCustoms")
     async with bot:
         await bot.start(os.getenv("TOKEN"))
 
 
-if os.getenv("DEV") == "TRUE":
-    bot.run(os.getenv("TOKEN"))
-else:
-    asyncio.run(main())
+# if os.getenv("DEV") == "TRUE":
+#     bot.run(os.getenv("TOKEN"))
+# else:
+asyncio.run(main())
