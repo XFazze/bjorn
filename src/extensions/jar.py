ac587b4af14461237ccc612b71c411a41abec8a4
trockeltum <abbe.breitholtz@gmail.com>
2024-04-25 21:01:06 +0200

    Fixed A jar

--- src/extensions/jar.py
+++ src/extensions/jar.py
@@ -1,50 +1,76 @@
 from discord.ext import commands
 import discord
-import random
-import math
-from typing import Literal
 import time
 
 #import lib.persmissions as permissions
 from lib.jar import Database
+allowed_user_id = 220607888459038721
 
-import sqlite3
-
-def connect():
-    conn = sqlite3.connect('data/league.sqlite')
-    cursor = conn.cursor()
-    cursor.execute("SELECT discord_id, money, timestamp FROM 2024")
-    rows = cursor.fetchall()
-    
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return rows
-
-
-class jar():
+class jar(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.db = Database(bot, "data/jar.sqlite")
+        self.db = Database(bot, "jar.sqlite")
     
-    @commands.hybrid_group(name="jar", description="A pot of money for the server")
+    @commands.hybrid_group(
+            name="jar", description="A pot of money for the server"
+            )
     async def jar(self, ctx: commands.Context):
         pass
     
     @jar.command(
-        name="Pot",
-        description="Says how much in total there is in the pot",
-    )
-    async def pot():
-        rows = connect()
-        for money in rows:
-            pot += money[1]
-        print(pot)
+            name="pot",description="Says how much in total there is in the pot",
+            )
+    async def pot(self, ctx: commands.Context):
+        pot = self.db.pot_value()
+        total = 0
+        for value in pot:
+            total += int(value[0])
+        await ctx.reply(
+            embed = discord.Embed(title=f"The total pot right now is {total}kr"))
 
     @jar.command(
-        name="How Tilted",
-        description="Shows how much a certain person has tilted",
-    )
-    async def how_tilted():
-        rows = connect()
-        
+            name="how_flamed",description="Shows how much a certain person has flamed",
+            )
+    async def how_flamed(self, ctx: commands.Context, user: discord.Member | None = None):
+        if user is None:
+            user = ctx.message.author
+        flamed = self.db.how_flamed(user.id)
+        await ctx.reply(
+            embed = discord.Embed(title=f"The person needs to pay {flamed}kr")
+            )
+
+    @jar.command(
+            name="someone_flamed",description="Adds so that specifik person flamed",
+            )
+    async def add_tilt(self, ctx: commands.Context, user: discord.Member | None = None):
+        if user is None:
+            user = ctx.message.author
+        if ctx.author.id == allowed_user_id:
+            flamed = self.db.add_tilt(user.id)
+            await ctx.reply(
+                embed = discord.Embed(title=f"{user.name} has contributed to the pot")
+                )
+        else:
+            await ctx.reply(
+                embed = discord.Embed(title=f"U don't have permission for this")
+                )
+    
+async def setup(bot: commands.Bot):
+    await bot.add_cog(jar(bot))
+    
+    
+    
+    
+    
+    
+    
+    
+    '''
+    @jar.command(name="Remove flame",description="The flame that was addes was not right",)
+    async def remove_tilt(self, ctx: commands.Context, time: str):
+        remove = self.db.remove_tilt(time)
+        await ctx.reply(
+            embed = discord.Embed(title=f"The flame was succsecfully removed")
+            )
+    '''
+    
